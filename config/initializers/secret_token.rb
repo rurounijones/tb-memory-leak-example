6e71290178e1d21281e0af95b79447674b1e6265
Jeffrey Jones <jjones@toppan-f.co.jp>
2013-02-18 11:06:50 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TorqueboxMemoryLeak::Application.config.secret_token = '02ca96bcb35ee5830a6a6e40d764c0195eacee8e71598aacb63c12e47c99b2d695738af9a4ca2685931f9ed28aae9f7e71be8cd6821488c456535dfd88ae8ca2'
